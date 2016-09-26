10ebbfa8f539ca18312e3d02724831595f162b5e
unniabc03 <unniabc03@gmail.com>
2016-09-26 16:34:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article-one', function (req, res) {
+app.get('/ui/article-one.html', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
