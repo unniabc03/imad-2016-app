0144d90ca9319e6ded7941374ce6171c3a80eea3
unniabc03 <unniabc03@gmail.com>
2016-09-28 19:52:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//move the image
+var img = document.getElementById('madi');
+img.click = function() {
+    img.style.marginLeft="100px";
+};
