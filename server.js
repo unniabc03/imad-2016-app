eff0257cd14fb905d891b132d162a83669292dea
unniabc03 <unniabc03@gmail.com>
2016-09-28 18:43:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName = req.params.aticleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
