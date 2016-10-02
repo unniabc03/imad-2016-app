c926bf606999a29148efcb361d011eb13baced13
unniabc03 <unniabc03@gmail.com>
2016-10-02 13:34:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,6 +119,14 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+    var name = req.params.name;
+    names.push(name);
+    res.send(JSON.stringfy(names));
+    
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
