49e41ab25cc13aa2b4c18b3498a3967f9c0eb42f
unniabc03 <unniabc03@gmail.com>
2016-10-02 11:49:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -113,11 +119,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
