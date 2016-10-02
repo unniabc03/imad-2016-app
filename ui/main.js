8ffbbde604d60c37e85e50a18192fd8861ae3f86
unniabc03 <unniabc03@gmail.com>
2016-10-02 13:23:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,18 @@ button.onclick = function() {
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_butt');
+submit.onclick = function () {
+    //make a request to the server and send name
+    //capture a list of names and render as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
